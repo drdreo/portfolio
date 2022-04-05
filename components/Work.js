475c8db2b0541298d3b58fbda3af98a981b6de39
Andreas Hahn <drdreo@users.noreply.github.com>
2022-04-05 12:51:11 +0200

    Update Work.js

--- components/Work.js
+++ components/Work.js
@@ -6,15 +6,15 @@ export function Work() {
     const projects = [
         {
             title: "NIX Wallet",
-            description: "The first NIX web wallet for easy, seed-based browser access to your NIX coins.",
+            description: "The first NIX (now mute coin) web wallet for easy, seed-based browser access to your NIX coins. <angular>",
             url: "https://webwallet.nixplatform.io/",
             image: "/nix_wallet.png"
         },
         {
-            title: "RisikOnline",
-            description: "An HTML multiplayer game based on the ruleset of the board game Risk. Never released sadly.",
-            url: "https://risikonline.herokuapp.com/",
-            image: "/risk.png"
+            title: "Poker",
+            description: "A simple lobby based Poker game to play with your friends. Emerged due to COVID and the social distancing rules. Work still in progress <angular, nest.js>",
+            github: "https://github.com/drdreo/poker",
+            image: "/poker.png"
         },
         {
             title: "lit-scss-loader",
@@ -29,47 +29,53 @@ export function Work() {
         },
         {
             title: "Owe Drahn",
-            description: "A dice roller gambling game to play with your friends.",
+            description: "A dice roller gambling game to play with your friends. <react, socket.io>",
             url: "https://owe-drahn.herokuapp.com/",
             github: "https://github.com/drdreo/owe-drahn",
             image: "/owe-drahn.png"
         },
         {
-            title: "Poker",
-            description: "A simple lobby based Poker game to play with your friends. Emerged due to COVID and the social distancing rules. Work still in progress",
-            github: "https://github.com/drdreo/poker",
-            image: "/poker.png"
+            title: "RisikOnline",
+            description: "An HTML multiplayer game based on the ruleset of the board game Risk. Never released sadly. <angular, node, socket.io>",
+            url: "https://risikonline.herokuapp.com/",
+            image: "/risk.png"
         },
         {
             title: "fitzbrothers",
-            description: "The homepage for my friends from the fitzbrothers band.",
+            description: "The homepage for my friends from the fitzbrothers band. <nuxt.js>",
             url: "https://fitzbrothers.at/",
             github: "https://github.com/drdreo/fitzbrothers",
             image: "/fitz.png"
         },
         {
             title: "Fretello",
-            description: "The homepage and songs search application for Fretello.",
+            description: "The homepage and songs search application for Fretello. <angular>",
             url: "https://fretello.com/",
             image: "/fretello.png"
         },
         {
-            title: "Funkography",
-            description: "A photographer portfolio website. First prototype. Work still in progress.",
-            url: "https://funkography.netlify.app/",
-            github: "https://github.com/drdreo/funkography",
-            image: "/funky.png"
+            title: "RatCat",
+            description: "A hosted configuration service. Manage configuration in one place, in real-time. <angular, nx>",
+            github: "https://github.com/throw-if-null/cat",
+            image: "/ratcat.png"
         },
         {
             title: "Electro Ultraways",
-            description: "A ultraway slot machine. Fun project, created to experiment with React.",
+            description: "A ultraway slot machine. Fun project, created to experiment with React. <react>",
             url: "https://electro-slot.herokuapp.com/",
             github: "https://github.com/drdreo/electro-slot",
             image: "/slot.png"
         },
         {
+            title: "Funkography",
+            description: "A photographer portfolio website. First prototype. Work still in progress. <nuxt.js>",
+            url: "https://funkography.netlify.app/",
+            github: "https://github.com/drdreo/funkography",
+            image: "/funky.png"
+        },
+        {
             title: "Reddit Clone",
-            description: "A simple reddit clone. Mirrors the basic reader functionality of reddit with a straightforward interface.",
+            description: "A very simple reddit clone. <angular>",
             url: "https://better-reddit.herokuapp.com/",
             github: "https://github.com/drdreo/better-reddit",
             image: "/reddit.png"
