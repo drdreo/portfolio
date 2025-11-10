e9cf9d2095b28db3ca460b1969c0037aaa4ae9f8
DrDreo <code@drdreo.com>
2025-11-10 14:04:59 +0100

    extract project tags

--- data/projects.ts
+++ data/projects.ts
@@ -4,30 +4,34 @@ export interface Project {
     url?: string;
     image?: string;
     github?: string;
+    tags?: string[];
 }
 
 export const projects: Project[] = [
     {
         title: "NIX Wallet",
         description:
-            "The first NIX (now mute coin) web wallet for easy, seed-based browser access to your NIX coins. <angular>",
+            "The first NIX (now mute coin) web wallet for easy, seed-based browser access to your NIX coins.",
         url: "https://webwallet.nixplatform.io/",
         image: "/nix_wallet.png",
+        tags: ["angular"],
     },
     {
         title: "Poker",
         description:
-            "A simple lobby based Poker game to play with your friends. Emerged due to COVID and the social distancing rules. <angular, nest.js>",
+            "A simple lobby based Poker game to play with your friends. Emerged due to COVID and the social distancing rules.",
         github: "https://github.com/drdreo/poker",
         image: "/poker.png",
+        tags: ["angular", "nest.js"],
     },
     {
         title: "KCDice",
         description:
-            "A Kingdom Come Deliverance inspired multiplayer dice game. <angular, golang>",
+            "A Kingdom Come Deliverance inspired multiplayer dice game.",
         url: "https://kcdice.drdreo.com",
         image: "/kcdice.png",
         github: "https://github.com/drdreo/hub/tree/master/apps/gameserver/games/dicegame",
+        tags: ["angular", "golang"],
     },
     {
         title: "lit-scss-loader",
@@ -35,62 +39,63 @@ export const projects: Project[] = [
             "A webpack loader plugin for easier SCSS handling for LitElement. Average of 1500 downloads / week",
         url: "https://www.npmjs.com/package/lit-scss-loader",
         github: "https://github.com/drdreo/lit-scss-loader",
+        tags: ["webpack", "LitElement", "SCSS"],
     },
     {
         title: "Owe Drahn",
         description:
-            "A dice roller gambling game to play with your friends. <react, nest.js, Google OAuth>",
+            "A dice roller gambling game to play with your friends.",
         url: "https://owe-drahn.drdreo.com/",
         github: "https://github.com/drdreo/owe-drahn",
         image: "/owe-drahn.png",
+        tags: ["react", "nest.js", "Google OAuth"],
     },
     {
         title: "Habi",
         description:
-            "A habit tracker web app. Simply track anything. <angular, golang, auth0>",
+            "A habit tracker web app. Simply track anything.",
         github: "https://habi.drdreo.com",
         image: "/habi.png",
+        tags: ["angular", "golang", "auth0"],
     },
     {
         title: "Web Component prerendering",
         description:
             "My research environment to server-side render Web Components.",
         url: "https://github.com/drdreo/wc-prerender",
+        tags: [],
     },
     {
         title: "RisikOnline",
         description:
-            "An HTML multiplayer game based on the ruleset of the board game Risk. Never released sadly. <angular, node, socket.io>",
+            "An HTML multiplayer game based on the ruleset of the board game Risk. Never released sadly.",
         image: "/risk.png",
+        tags: ["angular", "node", "socket.io"],
     },
     {
         title: "fitzbrothers",
         description:
-            "The homepage for my friends from the fitzbrothers band. <nuxt.js>",
+            "The homepage for my friends from the fitzbrothers band.",
         url: "https://fitzbrothers.at/",
         github: "https://github.com/drdreo/fitzbrothers",
         image: "/fitz.png",
+        tags: ["nuxt.js"],
     },
     {
         title: "Fretello",
         description:
-            "The homepage and songs search application for Fretello. <angular>",
+            "The homepage and songs search application for Fretello.",
         url: "https://fretello.com/",
         image: "/fretello.png",
-    },
-    {
-        title: "RatCat",
-        description:
-            "A hosted configuration service. Manage configuration in one place, in real-time. <angular, nx>",
-        github: "https://github.com/throw-if-null/cat",
-        image: "/ratcat.png",
+        tags: ["angular"],
     },
     {
         title: "Electro Ultraways",
         description:
-            "A ultraway slot machine. Fun project, created to experiment with React. <react>",
+            "A ultraway slot machine. Fun project, created to experiment with React.",
         url: "https://slot.drdreo.com/",
         github: "https://github.com/drdreo/electro-slot",
-        image: "/slot.png",
+        image: "/slot.PNG",
+        tags: ["react"],
     },
 ];
