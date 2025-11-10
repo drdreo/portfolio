6c3270b26a170cf172edb459d81c7cd53ba4f961
DrDreo <code@drdreo.com>
2025-11-10 14:00:31 +0100

    migrate to TS

--- components/Welcome.tsx
+++ components/Welcome.tsx
@@ -1,12 +1,13 @@
+import { FC } from "react";
 import styles from "./Welcome.module.scss";
 
-export function Welcome() {
+export const Welcome: FC = () => {
     return (
         <div className={styles.wrapper}>
             <h1 className={styles.title}>
-                Welcome,<br/>
-                I am Andreas Hahn
+                Welcome,
+                <br />I am Andreas Hahn
             </h1>
         </div>
     );
-}
+};
